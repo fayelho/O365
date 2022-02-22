46067bcf6b7e2f8fdda02a2abf5e534e7dcf26bb
Fayçal Bouzid <fay.bouzid@outlook.com>
2022-02-22 17:47:55 +0100

    office 365 api for sending emails

---
+++ O365.Mail/AuthenticationProvider.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Graph;
+using Microsoft.Identity.Client;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace O365.Mail
+{
+    public class AuthenticationProvider : IAuthenticationProvider
+    {
+        private readonly IConfidentialClientApplication _clientApplication;
+        private readonly string[] _scopes;
+
+        public AuthenticationProvider(IConfidentialClientApplication clientApplication, string[] scopes)
+        {
+            _clientApplication = clientApplication;
+            _scopes = scopes;
+        }
+
+        // Getting authorization using the Access Token
+        public async Task AuthenticateRequestAsync(HttpRequestMessage request)
+        {
+            string token = await GetTokenAsync();
+            request.Headers.Authorization = new AuthenticationHeaderValue("bearer", token);
+        }
+
+        // Getting access Token
+        public async Task<string> GetTokenAsync()
+        {
+            AuthenticationResult authResult = null;
+            authResult = await _clientApplication.AcquireTokenForClient(_scopes).ExecuteAsync();
+
+            return authResult.AccessToken;
+        }
+    }
+}
---
+++ O365.Mail/O365.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Graph;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace O365.Mail
+{
+    public class O365 : IO365
+    {
+        private static GraphServiceClient _client;
+        private readonly IConfidentialClientApplication _cca;
+
+        public O365(string tenantId, string clientId, string clientSecret)
+        {
+            try
+            {
+                // Authentication to Azure AD
+                _cca = ConfidentialClientApplicationBuilder.Create(clientId)
+                    .WithTenantId(tenantId)
+                    .WithClientSecret(clientSecret)
+                    .Build();
+
+                // Default scopes are sufficient
+                List<string> scopes = new List<string>()
+                {
+                    "https://graph.microsoft.com/.default"
+                };
+
+                var authProvider = new AuthenticationProvider(_cca, scopes.ToArray());
+                _client = new GraphServiceClient(authProvider);
+            }
+
+            catch (Exception ex)
+            {
+                throw new Exception($"Error connecting the API, please check permissions.\n {ex.Message}", ex);
+            }
+        }
+
+        public async Task SendMail(Message message)
+        {
+            try
+            {
+                string sendinAdress = message.From.EmailAddress.Address;
+                await _client.Users[sendinAdress].SendMail(message, false).Request().PostAsync();
+            }
+
+            catch(Exception ex)
+            {
+                throw new Exception($"Error while sending email.\n {ex.Message}", ex);
+            }
+        }
+    }
+}
